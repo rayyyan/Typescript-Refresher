650e2b54a4cf5985ff63111c5570fb109c750053
ibenchi <horizen27@gmail.com>
2022-03-01 21:28:04 +0000

    Void Type

--- app.ts
+++ app.ts
@@ -1,18 +1,9 @@
-type Combinable = number | string
-
-function combine(input1: Combinable, input2: Combinable) {
-  let result
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2
-  } else {
-    result = input1.toString() + input2.toString()
-  }
-
-  return result
+function add(n1: number, n2: number) {
+  return n1 + n2
 }
-//Ages
-const combineAges = combine(30, 26)
-console.log(combineAges)
-//Names
-const combineNames = combine("Ibenchi", "Rayan")
-console.log(combineNames)
+
+function printResult(num: number) {
+  console.log("Result" + num)
+}
+
+printResult(add(5, 12))
