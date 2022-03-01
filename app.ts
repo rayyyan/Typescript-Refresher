d35ffa9aa6907f9726496130d4304cda08f16efc
ibenchi <horizen27@gmail.com>
2022-03-01 20:22:45 +0000

    Enums Type

--- app.ts
+++ app.ts
@@ -1,20 +1,15 @@
-const person: {
-  name: string
-  age: number
-  hobbies: string[]
-  role: [number, string] //Tuples
-} = {
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+const person = {
   name: "Rayan",
   age: 24,
   hobbies: ["Sports", "Coding"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 }
-person.role[0] = 10
-person.role[1] = "Admin"
-let favActivities: string[]
-favActivities = ["Sports"]
-console.log(person.name)
 
-for (const hobby of person.hobbies) {
-  console.log(hobby)
+if (person.role === Role.AUTHOR) {
+  console.log("is admin")
 }
