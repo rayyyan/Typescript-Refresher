82c99c0891dbfa461163c614b7f5ac75f84e605f
ibenchi <horizen27@gmail.com>
2022-03-01 15:09:20 +0000

    Number Type

--- app.ts
+++ app.ts
@@ -1,1 +1,8 @@
-console.log("My code is awesome...")
+function add(n1: number, n2: number) {
+  return n1 + n2
+}
+
+const num1 = 5
+const num2 = 2.5
+const result = add(num1, num2)
+console.log(result)
