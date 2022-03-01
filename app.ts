4b1852ef7ca1c4ca6a738b44472dae187e96cae1
ibenchi <horizen27@gmail.com>
2022-03-01 21:53:49 +0000

    Function Callbacks

--- app.ts
+++ app.ts
@@ -5,6 +5,15 @@ function add(n1: number, n2: number) {
 function printResult(num: number) {
   console.log("Result" + num)
 }
+
+function addHandle(n1: number, n2: number, cb: (a: number) => void) {
+  const result = n1 + n2
+  cb(result)
+}
 let combineValues: (a: number, b: number) => number
 combineValues = add
 printResult(combineValues(5, 12))
+
+addHandle(10, 20, (result) => {
+  console.log(result)
+})
