42405b7a8d5943a12c6f6857ce9ceec9f2dbe661
ibenchi <horizen27@gmail.com>
2022-03-01 19:22:01 +0000

    Array Type

--- app.ts
+++ app.ts
@@ -1,10 +1,13 @@
-// const person: { name: string; age: number } = {
-//   name: "Rayan",
-//   age: 40,
-// }
 const person = {
   name: "Rayan",
   age: 24,
+  hobbies: ["Sports", "Coding"],
 }
 
+let favActivities: string[]
+favActivities = ["Sports"]
 console.log(person.name)
+
+for (const hobby of person.hobbies) {
+  console.log(hobby)
+}
