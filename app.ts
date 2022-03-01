17793c66696c78246a3b7764ae51b98f9c0ee80e
ibenchi <horizen27@gmail.com>
2022-03-01 15:24:30 +0000

    Boolean Type

--- app.ts
+++ app.ts
@@ -1,8 +1,13 @@
-function add(n1: number, n2: number) {
-  return n1 + n2
+function add(n1: number, n2: number, showResult: boolean) {
+  if (showResult) {
+    console.log(n1 + n2)
+  } else {
+    return n1 + n2
+  }
 }
 
 const num1 = 5
 const num2 = 2.5
-const result = add(num1, num2)
+const printResult = true
+add(num1, num2, printResult)
 console.log(result)
