6b930c5b93b73bfce737b427ccfa6991be6c470e
ibenchi <horizen27@gmail.com>
2022-03-01 20:03:10 +0000

    Tuples Type

--- app.ts
+++ app.ts
@@ -1,9 +1,16 @@
-const person = {
+const person: {
+  name: string
+  age: number
+  hobbies: string[]
+  role: [number, string] //Tuples
+} = {
   name: "Rayan",
   age: 24,
   hobbies: ["Sports", "Coding"],
+  role: [2, "author"],
 }
-
+person.role[0] = 10
+person.role[1] = "Admin"
 let favActivities: string[]
 favActivities = ["Sports"]
 console.log(person.name)
