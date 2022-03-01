aff70babc3a8a90430caf8d39023e6e44862a7ff
ibenchi <horizen27@gmail.com>
2022-03-01 20:41:00 +0000

    Union Type

--- app.ts
+++ app.ts
@@ -1,15 +1,16 @@
-enum Role {
-  ADMIN,
-  READ_ONLY,
-  AUTHOR,
-}
-const person = {
-  name: "Rayan",
-  age: 24,
-  hobbies: ["Sports", "Coding"],
-  role: Role.ADMIN,
-}
+function combine(input1: number | string, input2: number | string) {
+  let result
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2
+  } else {
+    result = input1.toString() + input2.toString()
+  }
 
-if (person.role === Role.AUTHOR) {
-  console.log("is admin")
+  return result
 }
+//Ages
+const combineAges = combine(30, 26)
+console.log(combineAges)
+//Names
+const combineNames = combine("Ibenchi", "Rayan")
+console.log(combineNames)
