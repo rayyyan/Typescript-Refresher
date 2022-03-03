640ca3638b6d4c0812063038cbb04b46aebb27d9
ibenchi <horizen27@gmail.com>
2022-03-03 10:42:37 +0000

    Read only +extends

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,10 +1,13 @@
-interface Greatable {
-  name: string
+interface Named {
+  readonly name: string
+}
+
+interface Greatable extends Named {
   greet(phrase: string): void
 }
 class Person implements Greatable {
   name: string
-  age: number
+  age = 30
   constructor(n: string) {
     this.name = n
   }
