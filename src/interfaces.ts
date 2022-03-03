0550c2a5789b52e2e7460dd3ad0b9ae2d00db3e8
ibenchi <horizen27@gmail.com>
2022-03-03 10:35:01 +0000

    Interface with class

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,15 +1,17 @@
-interface Person {
+interface Greatable {
   name: string
-  age: number
   greet(phrase: string): void
 }
-
-let user1: Person
-user1 = {
-  name: "Rayan",
-  age: 30,
-  greet(phrase: string) {
+class Person implements Greatable {
+  name: string
+  age: number
+  constructor(n: string) {
+    this.name = n
+  }
+  greet(phrase: string): void {
     console.log(phrase + " " + this.name)
-  },
+  }
 }
+let user1: Greatable
+user1 = new Person("Max")
 user1.greet("How is life")
