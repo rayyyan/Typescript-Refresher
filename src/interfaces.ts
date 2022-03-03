3d7ffcf033653edf1ae5fc77e40697dc18c302b2
ibenchi <horizen27@gmail.com>
2022-03-03 19:58:16 +0000

    typeGuards

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,36 +1,37 @@
-//type AddFn = (a: number, b: number) => number
+// //type AddFn = (a: number, b: number) => number
 
-interface AddFn {
-  (a: number, b: number): number
-}
-let add: AddFn
-add = (n1: number, n2: number) => {
-  return n1 + n2
-}
-interface Named {
-  readonly name?: string
-  outputName?: string
-}
+// interface AddFn {
+//   (a: number, b: number): number
+// }
+// let add: AddFn
+// add = (n1: number, n2: number) => {
+//   return n1 + n2
+// }
+// interface Named {
+//   readonly name?: string
+//   outputName?: string
+// }
 
-interface Greatable extends Named {
-  greet(phrase: string): void
-}
-class Person implements Greatable {
-  name?: string
-  age = 30
-  constructor(n?: string) {
-    if (n) {
-      this.name = n
-    }
-  }
-  greet(phrase: string): void {
-    if (this.name) {
-      console.log(phrase + " " + this.name)
-    } else {
-      console.log("Hi!")
-    }
-  }
-}
-let user1: Greatable
-user1 = new Person()
-user1.greet("How is life")
+// interface Greatable extends Named {
+//   greet(phrase: string): void
+// }
+// class Person implements Greatable {
+//   name?: string
+//   age = 30
+//   constructor(n?: string) {
+//     if (n) {
+//       this.name = n
+//     }
+//   }
+//   greet(phrase: string): void {
+//     if (this.name) {
+//       0
+//       console.log(phrase + " " + this.name)
+//     } else {
+//       console.log("Hi!")
+//     }
+//   }
+// }
+// let user1: Greatable
+// user1 = new Person()
+// user1.greet("How is life")
