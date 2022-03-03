147b8587697fa5de8587ccb06772082bae9d94a8
ibenchi <horizen27@gmail.com>
2022-03-03 11:31:09 +0000

    optional Param

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -8,22 +8,29 @@ add = (n1: number, n2: number) => {
   return n1 + n2
 }
 interface Named {
-  readonly name: string
+  readonly name?: string
+  outputName?: string
 }
 
 interface Greatable extends Named {
   greet(phrase: string): void
 }
 class Person implements Greatable {
-  name: string
+  name?: string
   age = 30
-  constructor(n: string) {
-    this.name = n
+  constructor(n?: string) {
+    if (n) {
+      this.name = n
+    }
   }
   greet(phrase: string): void {
-    console.log(phrase + " " + this.name)
+    if (this.name) {
+      console.log(phrase + " " + this.name)
+    } else {
+      console.log("Hi!")
+    }
   }
 }
 let user1: Greatable
-user1 = new Person("Max")
+user1 = new Person()
 user1.greet("How is life")
