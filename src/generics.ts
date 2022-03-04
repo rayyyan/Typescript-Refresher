a8d832a82be51a62f016640d723a19d5f8b45d7d
ibenchi <horizen27@gmail.com>
2022-03-04 11:05:51 +0000

    custom generics

--- src/generics.ts
+++ src/generics.ts
@@ -1,15 +1,37 @@
-const names = ["abdou", "rayan"]
-// Generic type
-const gNames: Array<string> = ["abdou", "rayan"]
-gNames[0].slice()
-//Promise
+// const names = ["abdou", "rayan"]
+// // Generic type
+// const gNames: Array<string> = ["abdou", "rayan"]
+// gNames[0].slice()
+// //Promise
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("Hello")
-  }, 2000)
-})
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("Hello")
+//   }, 2000)
+// })
 
-promise.then((data) => {
-  data.split(" ")
-})
+// promise.then((data) => {
+//   data.split(" ")
+// })
+
+// // Custom Generic
+
+// function merge<T, U>(objA: T, objB: U) {
+//   return Object.assign(objA, objB)
+// }
+// const merged = merge({ name: "rayan" }, { age: 24 })
+// console.log(merged.age)
+
+const rayan = {
+  name: "rayan",
+  age: 14,
+}
+const job = {
+  job: "student",
+}
+function merge<T, Z>(a: T, b: Z) {
+  return Object.assign(a, b)
+}
+const merged = merge(rayan, job)
+//const merged = Object.assign(rayan, job)
+console.log(merged.job)
