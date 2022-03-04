b3b25eb09ffbceacd0fc8942bcad8f2197d24dbe
ibenchi <horizen27@gmail.com>
2022-03-04 12:32:10 +0000

    generec function

--- src/generics.ts
+++ src/generics.ts
@@ -29,9 +29,24 @@ const rayan = {
 const job = {
   job: "student",
 }
-function merge<T extends object, Z extends object | number>(a: T, b: Z) {
+function merge<T extends object, Z extends object>(a: T, b: Z) {
   return Object.assign(a, b)
 }
 const merged = merge(rayan, job)
 //const merged = Object.assign(rayan, job)
 console.log(merged.job)
+
+interface Lengthy {
+  length: number
+}
+function countAndPrint<T extends Lengthy>(element: T) {
+  let descriptionTest = "Got no value."
+  if (element.length === 1) {
+    descriptionTest = "Got " + element.length + " element"
+  } else if (element.length) {
+    descriptionTest = "Got " + element.length + " element"
+  }
+  return [element, descriptionTest]
+}
+
+console.log(countAndPrint(["ff"]))
