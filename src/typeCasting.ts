a974fa8dad53d49315fd45cc6f575e8cd65d29a5
ibenchi <horizen27@gmail.com>
2022-03-04 08:31:15 +0000

    Type Casting

---
+++ src/typeCasting.ts
@@ -0,0 +1,8 @@
+// const userInputElement = <HTMLInputElement>(
+//   document.getElementById("user-input")!
+// )
+const userInputElement = document.getElementById(
+  "user-input"
+)! as HTMLInputElement
+
+userInputElement.value = "Hi you "
