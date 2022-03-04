11266826a33f7db988f29c7260886976dac4719b
ibenchi <horizen27@gmail.com>
2022-03-04 08:44:41 +0000

    Index Props

--- src/typeCasting.ts
+++ src/typeCasting.ts
@@ -6,3 +6,16 @@ const userInputElement = document.getElementById(
 )! as HTMLInputElement
 
 userInputElement.value = "Hi you "
+
+// Index Props
+interface ErrorContainer {
+  //{email ;"Not a valid email", username:"Must start with a character"}
+  [prop: string]: string
+}
+
+const errorBag: ErrorContainer = {
+  email: "Not a valid email",
+  username: "Must start with a capital character",
+  id: "5854",
+  12: "i can put a number as a key",
+}
