6f2b47c988acea90d6fe64728947d7cc4fc1be9b
ibenchi <horizen27@gmail.com>
2022-03-12 14:38:05 +0000

    basics

---
+++ basics/basics.ts
@@ -0,0 +1,33 @@
+let userNames: string = "Max"
+let matterway: boolean = true
+
+userName += " you are hired"
+console.log(userNames)
+
+let myRegx = /foo/
+//Arrays
+const names: string[] = userNames.split(" ")
+const myValues: Array<string> = ["rayan", "mama"]
+// myValues.push()
+interface Person {
+  name: string
+  age: number
+}
+const myPerson: Person = {
+  name: "rayan",
+  age: 25,
+}
+
+const ids: Record<number, string> = {
+  10: "a",
+  20: "b",
+}
+ids[30] = "r"
+
+for (let i = 0; i < 10; i++) {
+  console.log(i)
+}
+//
+;["rayan", 5, true].forEach((e) => {
+  console.log(e)
+})
