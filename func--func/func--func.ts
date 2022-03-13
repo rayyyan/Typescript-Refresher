e5aa2d45db674be10260b7b2343a55f0f89646a2
ibenchi <horizen27@gmail.com>
2022-03-13 10:29:48 +0000

    func-func return func

---
+++ func--func/func--func.ts
@@ -0,0 +1,29 @@
+export function printToFile(text: string, callback: () => void): void {
+  console.log(text)
+  callback()
+}
+//function type
+interface MutationFunc {
+  (n: number): number
+}
+type MutationFuncType = (v: number) => number
+export function arrayMutate(
+  numbers: number[],
+  mutate: MutationFuncType
+): number[] {
+  return numbers.map(mutate)
+}
+const meta = (n: number): number => {
+  return n * 10
+}
+const myNewFunc: MutationFuncType = (v: number) => v * 5
+console.log(arrayMutate([1, 2, 3], meta))
+
+//func return a func
+
+export function createAdder(num: number) {
+  return (val: number) => num + val
+}
+const addOne = createAdder(1)
+console.log(addOne(55))
+console.log(addOne(5))
