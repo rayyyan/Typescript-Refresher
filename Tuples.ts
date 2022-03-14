8e07de31b614a401daacb2710353e222817d37e4
ibenchi <horizen27@gmail.com>
2022-03-14 22:37:13 +0000

    Tuple

---
+++ Tuples.ts
@@ -0,0 +1,9 @@
+type ThreeDCoordinate = [x: number, y: number, z: number]
+function addThree(
+  c1: ThreeDCoordinate,
+  c2: ThreeDCoordinate
+): ThreeDCoordinate {
+  return [c1[0] + c2[0], c1[1] + c2[1], c1[2] + c2[2]]
+}
+
+console.log(addThree([0, 100, 0], [10, 20, 30]))
