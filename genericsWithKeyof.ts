9911a75d9cc4e356ca89b97ef1e5e3a3ac78747c
ibenchi <horizen27@gmail.com>
2022-03-15 13:49:16 +0000

    Event map with Keys

--- genericsWithKeyof.ts
+++ genericsWithKeyof.ts
@@ -11,3 +11,28 @@ const dogs = [
 
 console.log(pluck(dogs, "age"))
 console.log(pluck(dogs, "name"))
+
+// Event Map
+
+interface BaseEvent {
+  time: number
+  user: string
+}
+
+interface EventMap {
+  addToCart: BaseEvent & { quantity: number; productID: string }
+  checkout: BaseEvent
+}
+const sendEvent = <Name extends keyof EventMap>(
+  name: Name,
+  data: EventMap[Name]
+): void => {
+  console.log(name, data)
+}
+sendEvent("addToCart", {
+  time: 5,
+  user: "rayan",
+  productID: "foo",
+  quantity: 1,
+})
+sendEvent("checkout", { time: 5, user: "rayan" })
