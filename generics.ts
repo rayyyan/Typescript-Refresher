bc1a04f350e34da18fb5fbd70400b0af5f09f8fc
ibenchi <horizen27@gmail.com>
2022-03-15 10:45:18 +0000

    generics Ranking

--- generics.ts
+++ generics.ts
@@ -18,3 +18,33 @@ const [mate, setMate] = state<string | null>(null)
 console.log(mate())
 setMate("string")
 console.log(mate())
+
+//Ranker
+interface Rank<RankItem> {
+  item: RankItem
+  rank: number
+}
+const ranker = <RankItem>(
+  items: RankItem[],
+  rank: (v: RankItem) => number
+): RankItem[] => {
+  const ranks: Rank<RankItem>[] = items.map((item) => ({
+    item,
+    rank: rank(item),
+  }))
+  console.log(ranks)
+  ranks.sort((a, b) => a.rank - b.rank)
+  console.log(ranks)
+  console.log(ranks.map((rank) => rank.item))
+  return ranks.map((rank) => rank.item)
+}
+interface Marvel {
+  name: string
+  hp: number
+}
+const heros: Marvel[] = [
+  { name: "thor", hp: 100 },
+  { name: "tony", hp: 0 },
+]
+
+ranker(heros, ({ hp }) => hp)
