5d13ec4c2beca1d6a44cb8d9d36ba7446fe48bfe
ibenchi <horizen27@gmail.com>
2022-03-15 13:38:03 +0000

    Pluck Keys

---
+++ genericsWithKeyof.ts
@@ -0,0 +1,13 @@
+const pluck = <DataType, KeyType extends keyof DataType>(
+  items: DataType[],
+  key: KeyType
+): DataType[KeyType][] => {
+  return items.map((item) => item[key])
+}
+const dogs = [
+  { name: "Mimi", age: 12 },
+  { name: "LG", age: 12 },
+]
+
+console.log(pluck(dogs, "age"))
+console.log(pluck(dogs, "name"))
