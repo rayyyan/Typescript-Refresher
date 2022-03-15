711a2ad54acec7f3d310290ef6d38ee0cd7ac45e
ibenchi <horizen27@gmail.com>
2022-03-15 14:03:32 +0000

    utility Partial

---
+++ utility-types-1.ts
@@ -0,0 +1,21 @@
+interface MyUser {
+  name: string
+  id: string
+  email?: string
+}
+type MyUserOptionals = Partial<MyUser>
+const merges = (user: MyUser, overrides: MyUserOptionals): MyUser => {
+  return { ...user, ...overrides }
+}
+console.log(
+  merges(
+    {
+      name: "Rayan",
+      id: "foo",
+      email: "gmail.com",
+    },
+    {
+      email: "gmail.cosssm",
+    }
+  )
+)
