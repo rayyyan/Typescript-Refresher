f69761e41c2f2791fae9dffead39f793cc1f868d
ibenchi <horizen27@gmail.com>
2022-03-15 12:17:27 +0000

    map

--- challengeTwo.ts
+++ challengeTwo.ts
@@ -8,11 +8,18 @@ const myForEach = <T>(items: T[], forEachFunc: (v: T) => void): void => {
 myForEach(["1", "2", "3"], (v) => console.log(`forEach ${v}`))
 // filter
 
-// const myFilter = <T>(items: T[], filterFunc: (v: T) => boolean): T[] => {
-//   return items.reduce((a, v) => (filterFunc(v) ? [...a, v] : a), [])
-// }
-
-function myFilter<T>(items: T[], filterFunc: (v: T) => boolean): T[] {
-  return items.reduce<T[]>((a, v) => (filterFunc(v) ? [...a, v] : a), [])
+const myFilter = <T>(items: T[], filterFunc: (v: T) => boolean): T[] => {
+  return items.reduce((a, v) => (filterFunc(v) ? [...a, v] : a), [] as T[])
 }
+
+// function myFilter<T>(items: T[], filterFunc: (v: T) => boolean): T[] {
+//   return items.reduce<T[]>((a, v) => (filterFunc(v) ? [...a, v] : a), [])
+// }
 console.log(myFilter([4, 2, 3, 9, 5], (v) => v % 2 === 0))
+
+//myMap
+
+const myMap = <T, K>(items: T[], myMapFunc: (v: T) => K): K[] => {
+  return items.reduce((a, v) => [...a, myMapFunc(v)], [] as K[])
+}
+console.log(myMap([1, 5, 6, 88, 74, 21, 47], (v) => (v * 10).toString()))
