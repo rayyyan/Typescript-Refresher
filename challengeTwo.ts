3d965cb5d5eaa89aceb7a422ff773fc29ffe9ab2
ibenchi <horizen27@gmail.com>
2022-03-15 12:06:39 +0000

    filter

--- challengeTwo.ts
+++ challengeTwo.ts
@@ -6,3 +6,13 @@ const myForEach = <T>(items: T[], forEachFunc: (v: T) => void): void => {
 }
 
 myForEach(["1", "2", "3"], (v) => console.log(`forEach ${v}`))
+// filter
+
+// const myFilter = <T>(items: T[], filterFunc: (v: T) => boolean): T[] => {
+//   return items.reduce((a, v) => (filterFunc(v) ? [...a, v] : a), [])
+// }
+
+function myFilter<T>(items: T[], filterFunc: (v: T) => boolean): T[] {
+  return items.reduce<T[]>((a, v) => (filterFunc(v) ? [...a, v] : a), [])
+}
+console.log(myFilter([4, 2, 3, 9, 5], (v) => v % 2 === 0))
