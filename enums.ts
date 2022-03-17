d2a6e111a1e1c038a39348d04e2a65d8d100a116
ibenchi <horizen27@gmail.com>
2022-03-17 20:07:37 +0000

    literal types

--- enums.ts
+++ enums.ts
@@ -9,3 +9,14 @@ enum LoadingState {
 //Boolean
 const isLoading = (state: LoadingState) => state === LoadingState.loading
 console.log(isLoading(LoadingState.loaded))
+
+//LIterral Types
+
+function rollDice(dice: 1 | 2 | 3): number {
+  let pip = 0
+  for (let i = 0; i < dice; i++) {
+    pip += Math.floor(Math.random() * 5) + 1
+  }
+  return pip
+}
+console.log(rollDice(3))
