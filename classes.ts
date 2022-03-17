9f1e9810317eefab6915fce1501852595fffbea7
ibenchi <horizen27@gmail.com>
2022-03-17 21:28:26 +0000

    Protected

--- classes.ts
+++ classes.ts
@@ -4,16 +4,27 @@ interface DataBase {
   get(id: string): string
   set(id: string, value: string): void
 }
-
+interface Persistable {
+  saveToString(): string
+  restoreFromString(storedState: string): void
+}
 class InMemoryDb implements DataBase {
   constructor() {}
-  private db: Record<string, string> = {}
+  protected db: Record<string, string> = {}
   get(id: string): string {
     return this.db[id]
   }
-
   set(id: string, value: string): void {
     this.db[id] = value
+  }
+}
+//Persistable in memory Db
+class PersistableMemoryDb extends InMemoryDb implements Persistable {
+  saveToString(): string {
+    return JSON.stringify(this.db)
+  }
+  restoreFromString(storedState: string): void {
+    this.db = JSON.parse(storedState)
   }
 }
 const myDb = new InMemoryDb()
