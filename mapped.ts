61337441c2ec43f9b00430f3177ce831c7aeaa4f
ibenchi <horizen27@gmail.com>
2022-03-19 22:27:32 +0000

    template literals

--- mapped.ts
+++ mapped.ts
@@ -24,3 +24,21 @@ type OptionsFlags<Type> = {
   [Property in keyof Type]: null
 }
 type DogInfoOption = OptionsFlags<DogInfo>
+
+type Listeners<Type> = {
+  [Property in keyof Type as `on${Capitalize<string & Property>}Change`]: (
+    newValue: Type[Property]
+  ) => void
+}
+function listenToObject<T>(obj: T, listeners: Listeners<T>): void {
+  throw "Needs to be implemented"
+}
+const lg: DogInfo = {
+  name: "LG",
+  age: 13,
+}
+type DogInfoListeners = Listeners<DogInfo>
+listenToObject(lg, {
+  onNameChange: (v: string) => {},
+  onAgeChange: (v: number) => {},
+})
