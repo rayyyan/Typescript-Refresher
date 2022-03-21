4dd0e860a0ba90c72892373c1a0344a889951a69
ibenchi <horizen27@gmail.com>
2022-03-21 21:15:47 +0000

    another abstract method

--- abstract.ts
+++ abstract.ts
@@ -2,9 +2,10 @@ abstract class StreetFighter {
   constructor() {}
   move() {}
   fight() {
-    console.log(`atack with ${this.getSpecialAttack()}`)
+    console.log(`${this.getName()} atack with ${this.getSpecialAttack()}`)
   }
   abstract getSpecialAttack(): string
+  abstract getName(): string
 }
 //Cant instantiate
 //const ryuCant = new StreetFighter()
@@ -13,6 +14,9 @@ class Ryu extends StreetFighter {
   //when inherits a class that have abstract method it means that it's required
   getSpecialAttack(): string {
     return "Hadoken"
+  }
+  getName(): string {
+    return "Ryu"
   }
 }
 const ryu = new Ryu()
